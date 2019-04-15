b417ada090534e5ceeaf25fce22381939aaf21c0
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-04-15 11:56:27 +0300

    Add OOAD demo: DI  pattern.

--- src/main/java/com/acme/banking/dbo/ooad/service/TransferService.java
+++ src/main/java/com/acme/banking/dbo/ooad/service/TransferService.java
@@ -3,8 +3,23 @@ package com.acme.banking.dbo.ooad.service;
 import com.acme.banking.dbo.ooad.domain.Account;
 
 public class TransferService {
-    private CurrencyService currencyService =
-            Registry.getConcreteFactory("component-id").getCurrencySerice();
+    //Field DI
+    private CurrencyService currencyService;
+
+    //Constructor DI
+    public TransferService(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    //Setter DI
+    public void setCurrencyService(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    //Method DI
+    public void save(@Valid @x @y @z Request rq) {
+
+    }
 
     public void withdraw(Account from, double rurAmount) { //PL/SQL
         //polymorphism
